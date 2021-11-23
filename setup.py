8acf291c58cdc3108524d2902e35f068b858f57e
cchowthi <79476431+cchowthi@users.noreply.github.com>
2021-11-23 14:18:28 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="puts",
-    version="0.0.2",
+    version="0.0.3",
     author="Christal Chowthi",
     author_email="christal.chowthi@gilead.com",
     description="Python Utility Testing System",
